2b8e102a9f7aba05ecfbaa6207fae1d6d29729f5
Jack Birchall <jackbirchall55@gmail.com>
2024-03-23 16:36:44 +0000

    Added exception handling to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,14 @@ public class App {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a word to check if it is a palindrome:");
-        String word = input.nextLine();
-        System.out.println(PalindromeChecker.isPalindrome(word) ? "It is a palindrome!" : "It is not a palindrome!");
+
+        try {
+            System.out.println("Enter a word to check if it is a palindrome:");
+            String word = input.nextLine();
+            System.out.println(PalindromeChecker.isPalindrome(word) ? "It is a palindrome!" : "It is not a palindrome!");
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+        }
 
     }
 
