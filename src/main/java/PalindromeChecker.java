5117ccfcb02a77b35edbcf2cb9004583a4c8815c
Jack Birchall <jackbirchall55@gmail.com>
2024-03-23 16:11:19 +0000

    Added isPalindrome method to PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,2 +1,11 @@
 public class PalindromeChecker {
+
+    private boolean isPalindrome(String word) {
+        if(word == null) {
+            return false;
+        } else {
+            return word.equals(new StringBuilder(word).reverse().toString());
+        }
+    }
+
 }
