dd896e5cd0daa4433e0a9d941daed4a88d841086
Jack Birchall <jackbirchall55@gmail.com>
2024-03-23 16:35:04 +0000

    Added main method in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class App {
 
     public static void main(String[] args) {
 
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word to check if it is a palindrome:");
+        String word = input.nextLine();
+        System.out.println(PalindromeChecker.isPalindrome(word) ? "It is a palindrome!" : "It is not a palindrome!");
 
     }
 
