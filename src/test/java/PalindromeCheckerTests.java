cdc35b7964157fe93132a21e45b3c023b974d5e6
Jack Birchall <jackbirchall55@gmail.com>
2024-03-23 16:17:03 +0000

    Added testIsPalindromeReturnsFalseForNonPalindrome and testIsPalindromeReturnsFalseForNull methods to PalindromeCheckerTests.java

--- src/test/java/PalindromeCheckerTests.java
+++ src/test/java/PalindromeCheckerTests.java
@@ -1,12 +1,29 @@
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
 
 public class PalindromeCheckerTests {
     
     @Test
+    @DisplayName("isPalindrome returns true for palindrome")
     public void testIsPalindromeReturnsTrueForPalindrome() {
         PalindromeChecker checker = new PalindromeChecker();
         assertTrue(checker.isPalindrome("racecar"));
+    }
+
+    @Test
+    @DisplayName("isPalindrome returns false for non-palindrome")
+    public void testIsPalindromeReturnsFalseForNonPalindrome() {
+        PalindromeChecker checker = new PalindromeChecker();
+        assertFalse(checker.isPalindrome("hello"));
+    }
+
+    @Test
+    @DisplayName("isPalindrome returns false for null")
+    public void testIsPalindromeReturnsFalseForNull() {
+        PalindromeChecker checker = new PalindromeChecker();
+        assertFalse(checker.isPalindrome(null));
     }
 
 }
