f0e401ee18347aae393595e3810d585b76829431
Jack Birchall <jackbirchall55@gmail.com>
2024-03-23 17:00:18 +0000

    Updated PalindromeChecker.java to ignore whitespace in string and to return false for empty strings.

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,10 +1,10 @@
 public class PalindromeChecker {
 
     static boolean isPalindrome(String word) {
-        if(word == null) {
+        if(word == null || word.trim().isEmpty()) {
             return false;
         } else {
-            return word.toLowerCase().equals(new StringBuilder(word.toLowerCase()).reverse().toString());
+            return word.toLowerCase().replaceAll("\\s+", "").equals(new StringBuilder(word.toLowerCase().replaceAll("\\s+", "")).reverse().toString());
         }
     }
 
